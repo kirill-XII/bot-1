a808bc6123c49302c36d0a3b1b7ebb5ca59560c1
kirill <116079986+kirill-XII@users.noreply.github.com>
2025-01-11 19:13:37 +0300

    Part_2

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\kiril\Desktop\Traning python\venv
+set VIRTUAL_ENV=D:\Папка С Папками\Traning python\bot-1\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
